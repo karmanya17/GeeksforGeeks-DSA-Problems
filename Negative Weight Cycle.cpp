8eaf408bb36fc7b58197f3a54b74f331758764de
karmanya17 <53150531+karmanya17@users.noreply.github.com>
2021-07-29 23:02:15 +0530

    Negative Weight Cycle-Bellmon Floyd Algorithm

---
+++ Negative Weight Cycle.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
+	    vector<int>dis(n,INT_MAX);
+	    dis[0]=0;
+	    for(int count=0;count<n-1;count++)
+	    {
+	        for(auto it:edges)
+	        {
+	            int u=it[0];
+	            int v=it[1];
+	            int wt=it[2];
+	            if(dis[u]!=INT_MAX&&dis[v]>dis[u]+wt)
+	            {
+	                dis[v]=dis[u]+wt;
+	            }
+	        }
+	    }
+	    for(auto it:edges)
+	    {
+	        int u=it[0];
+	        int v=it[1];
+	        int w=it[2];
+
+	        if(dis[u]!=INT_MAX&&dis[v]>dis[u]+w)
+	        {
+
+	            return 1;
+	        }
+	    }
+
+	    return 0;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>edges;
+		for(int i = 0; i < m; i++){
+			int x, y, z;
+			cin >> x >> y >> z;
+			edges.push_back({x,y,z});
+		}
+		Solution obj;
+		int ans = obj.isNegativeWeightCycle(n, edges);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
